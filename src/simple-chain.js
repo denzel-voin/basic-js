c88d08c9aed23411acb712470940718814715aac
Denis Voinovich <denzelvojn@gmail.com>
2023-04-14 17:30:16 +0300

    feat: add chainmaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+  createChain() {
+    this.chain = [];
+    return this;
+  },
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length;
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    this.chain.push(`( ${value !== undefined ? value : ''} )`);
+    return this;
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    if (typeof position !== 'number' || position < 1 || position > this.chain.length) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.chain.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    const result = this.chain.join('~~');
+    this.chain = [];
+    return result;
     // remove line with error and write your code here
   }
 };
