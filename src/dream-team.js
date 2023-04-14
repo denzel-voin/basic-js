f6998bacb05952e17a9af31e57f0c631f3c0e386
Denis Voinovich <denzelvojn@gmail.com>
2023-04-14 09:58:13 +0300

    feat: add dream team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,8 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const team = members.map((name) => {
+    if (typeof name === 'string') {
+      return name.trim()[0].toUpperCase();
+    }
+  }).filter(Boolean);
+  return team.sort().join('');
   // remove line with error and write your code here
 }
 
