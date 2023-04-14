74d31598375a79ccac9841a44796508ae61cb265
Denis Voinovich <denzelvojn@gmail.com>
2023-04-14 09:32:56 +0300

    feat: add date sample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  const activity = parseFloat(sampleActivity);
+  if (isNaN(activity) || activity <= 0 || activity >= MODERN_ACTIVITY) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / k);
+  return age;
   // remove line with error and write your code here
 }
 
